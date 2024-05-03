e8299b4851156dc55ecab9d0f4446764631f9afd
Fajrul <fajrul.falah28@gmail.com>
2024-05-03 14:42:12 +0700

    Updated files

--- script.js
+++ script.js
@@ -19,9 +19,25 @@ $(document).ready(function () {
     window.location.href = "post.html";
   });
 
-  $("button[type='submit']").click(function () {
-    window.location.href = "dashboard.html";
-  });
+  $('#loginForm').submit(function (event) {
+    event.preventDefault();
+    var username = $('input[type="text"]').val();
+    var password = $('input[type="password"]').val();
+
+    if (username === 'username' && password === 'password') {
+        alert('Login successful');
+
+        setTimeout(function() {
+          window.location.href = 'dashboard.html';
+      }, 2000);
+    } else {
+        $('#failedModal').removeClass('hidden');
+    }
+});
+
+$('#closeModal').click(function () {
+    $('#failedModal').addClass('hidden');
+});
 
   $('#account-button').on('click', function() {
     window.location.href = 'account.html';
